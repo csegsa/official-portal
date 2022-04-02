af6ba88586f08d97771e565db72241a8e889ff11
incedendo <nguyenminhkiet94@gmail.com>
2022-04-02 12:43:57 -0500

    kiet updates

--- csegsa_website/src/components/pages/Home.js
+++ csegsa_website/src/components/pages/Home.js
@@ -1,26 +1,61 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 import TeamBoard from '../team/TeamBoard';
+import { auth, signInWithGoogle, signOutOfGoogle } from '../../Firebase';
 
 const Home = (props) => {
 
+    const [authenticated, setAuthenticated] = useState(false);
     const { username, setUsername } = useContext(AppContext);
 
-    const handleChange = (e) => {
-        console.log(e.target.value);
-        setUsername(e.target.value);
-    }
+    auth.onAuthStateChanged( user => {
+        if (user) {
+            console.log("user logged in before");
+            setAuthenticated(true);
+        } else {
+            console.log("user hasn't logged in");
+            setAuthenticated(false);
+        }
+    })
+
+    useEffect(() => {
+        if (authenticated) {
+            console.log("authenticad state changed recognized...");
+            setUsername(auth.currentUser.displayName);
+            // console.log(auth.currentUser.email);
+        } else {
+            setUsername("");
+        }
+    }, [authenticated]);
 
     return (
         <div className="">
             <div>This is the home page </div>
 
-            <div>Enter your username here: <input onChange={handleChange}></input></div>
+            {/* <div>Enter your username here: <input onChange={handleChange}></input></div> */}
 
-            {username !== '' && 
-            <div>Hello, {username}</div>}
-
+            {authenticated && 
+                <div>
+                    <h2>{username}</h2>
+                    {/* <h2>{localStorage.getItem("email")}</h2>
+                    <img src={localStorage.getItem("profilePic")}></img> */}
+                </div>
+            }
+            
+            
             <TeamBoard/>
+
+            {!authenticated && 
+                <button onClick={signInWithGoogle}>
+                    Sign in With Google
+                </button>
+            }
+
+            {authenticated && 
+                <button onClick={signOutOfGoogle}>
+                    Sign Out
+                </button>
+            }
         </div>
       );  
 }
