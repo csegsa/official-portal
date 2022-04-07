351a0827c665858a8bfc81744928ede5b6fffd7a
abhinand <abhinandsainasari@gmail.com>
2022-04-06 23:31:27 -0500

    disabled eslint for build temporarily

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   },
   plugins: ["react"],
   rules: {
-    "require-jsdoc" : "off"
+    "require-jsdoc" : "off",
+	  "prettier/prettier": ["error", { "singleQuote": true }]
   }
 }
