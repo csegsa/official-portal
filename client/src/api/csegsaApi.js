4a3b7c2a843c40f07f0712cc8bae2481cece7d0e
Himanshu Singh <heman.sam@gmail.com>
2022-04-21 13:24:46 -0500

    changed eventAPI

--- client/src/api/csegsaApi.js
+++ client/src/api/csegsaApi.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 
 export default axios.create({
-    // baseURL: "https://csegsa-portal.herokuapp.com/api",
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://csegsa-portal.herokuapp.com/api",
+    //baseURL: "http://localhost:5000/api",
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
