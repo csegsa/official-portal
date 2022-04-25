893dedc97c2c5bf9aba783d3922fcf4e6d6dbcfb
Incedendo <nguyenminhkiet94@gmail.com>
2022-04-25 13:05:52 -0500

    change port number back to 5000 in server

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.get('*', (req, res) => {
 });
 
 const CONNECT_URL = process.env.MONGODB_CONNECTION_STRING;
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 try {
   await mongoose.connect(CONNECT_URL, { useNewUrlParser: true, useUnifiedTopology: true });
