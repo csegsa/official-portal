964e7cc9f40cf33ce9e4187b15d57d72e5bc3cf2
abhinand <abhinandsainasari@gmail.com>
2022-04-30 17:29:27 -0500

    format fix

--- client/src/views/jobs/JobPostings.js
+++ client/src/views/jobs/JobPostings.js
@@ -1,16 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 // reactstrap components
-import { Button, Card, CardBody, CardFooter, CardTitle, Container, Row, Col } from 'reactstrap'
+import { Container } from 'reactstrap'
 
 // core components
 import MainNavbar from 'components/Navbars/MainNavbar.js'
 import JobPostingHeader from 'components/Headers/JobPostingHeader'
 import MainFooter from 'components/Footers/MainFooter'
 
-import JobsForm from './AddJobs.js'
 import JobListing from './JobListing.js'
-import csegsaApi from 'api/csegsaApi.js'
 import { Link } from 'react-router-dom'
 
 function JobPostings() {
