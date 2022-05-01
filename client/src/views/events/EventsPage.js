fc8edb7a0a2e5e5e24049497e111d46552935ccf
abhinand <abhinandsainasari@gmail.com>
2022-04-30 20:47:13 -0500

    contextual add

--- client/src/views/events/EventsPage.js
+++ client/src/views/events/EventsPage.js
@@ -1,26 +1,45 @@
 import React from 'react'
-
-// reactstrap components
 import { Container, Row, Col, Card } from 'reactstrap'
-
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import interactionPlugin from '@fullcalendar/interaction' // needed
 import listPlugin from '@fullcalendar/list' // For List View
-
-// core components
 import MainNavbar from 'components/Navbars/MainNavbar.js'
 import EventPageHeader from 'components/Headers/EventPageHeader'
 import MainFooter from 'components/Footers/MainFooter'
 import csegsaApi from 'api/csegsaApi.js'
 import { Link } from 'react-router-dom'
+import { auth } from '../userlogin/Firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'
 
 function EventsPage() {
   const [events, setEvents] = React.useState([])
   const [, setIsloaded] = React.useState(false)
   const [displayEvent, setDisplayEvent] = React.useState(null)
-
+  const [user, loading, error] = useAuthState(auth)
+  const [showAddEvent, setShowAddEvent] = React.useState(false)
   document.documentElement.classList.remove('nav-open')
+
+  React.useEffect(() => {
+    if (user) {
+      console.log('Checking if event can be added')
+      console.log(user, loading, error)
+      csegsaApi.get('/roles' + '?user=' + user.email).then(res => {
+        console.log(res.data)
+        if (res.data.role === 'admin') {
+          setShowAddEvent(true)
+          console.log('showAddEvent: ' + showAddEvent)
+        }
+      })
+    } else if (loading) {
+      console.log('Loading')
+      setShowAddEvent(false)
+    } else if (error) {
+      console.log('Error')
+      setShowAddEvent(false)
+    }
+  }, [user, loading, error])
+
   React.useEffect(() => {
     document.body.classList.add('profile-page')
     return function cleanup() {
@@ -30,32 +49,19 @@ function EventsPage() {
 
   React.useEffect(() => {
     csegsaApi.get('/events').then(res => {
-      console.log(res.data)
       setEvents(res.data)
       setIsloaded(true)
     })
   }, [])
 
   const viewEvent = arg => {
-    // console.log(arg.event) ;
     const filterId = arg.event.id
-    // console.log(events)
+
     const selectedEvent = events.filter(event => event._id === filterId)
-    // console.log("inside view event") ;
-    // console.log(selectedEvent) ;
+
     setDisplayEvent(selectedEvent)
   }
-  // console.log(eventsList) ;
 
-  // <[
-  //   { title: 'event 1', date: '2022-04-13' },
-  //   { title: 'event 2', date: '2022-04-09' },
-  //   { title: 'event 2', date: '2022-04-09' },
-  //   { title: 'event 2', date: '2022-04-09' },
-  //   { title: 'event 2', date: '2022-04-12' },
-  //   { title: 'event 2', date: '2022-04-13' },
-  //   { title: 'event 2', date: '2022-04-15' }
-  // ]>
   const eventsList = events.map(event => {
     return {
       id: event._id,
@@ -63,8 +69,6 @@ function EventsPage() {
       date: event.start_time
     }
   })
-
-  // console.log(eventsList) ;
 
   let cardContent = (
     <Card className="ml-auto mr-auto">
@@ -118,15 +122,18 @@ function EventsPage() {
                   events={eventsList}
                   eventClick={val => {
                     viewEvent(val)
-                    // console.log(displayEvent);
                   }}
                 />
               </Col>
               <Col md="4">
                 <Row>
-                  <Link to="/add-event" className="btn btn-danger">
-                    Add Event
-                  </Link>
+                  {showAddEvent ? (
+                    <Link to="/add-event" className="btn btn-danger">
+                      Add Event
+                    </Link>
+                  ) : (
+                    <></>
+                  )}
                 </Row>
                 {cardContent}
               </Col>
